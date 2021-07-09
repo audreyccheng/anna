2384f5a394cfb53586aa24532f6c271f185dbb11
Terrance Li <tyli@berkeley.edu>
2021-07-08 23:56:13 -0700

    Fix log node start script

--- scripts/start-log.sh
+++ scripts/start-log.sh
@@ -2,7 +2,7 @@ cp conf/anna-log.yml conf/anna-config.yml
 
 export SERIALIZABILITY_PROTOCOL="mvcc"
 export SERVER_TYPE="log"
-./build/target/kvs/anna-log &
+./build/target/kvs/anna-txn &
 S2PID=$!
 
 echo $S2PID
