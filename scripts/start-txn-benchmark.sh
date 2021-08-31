4385e1e6d7b0989d798cf2c47fe4079b9d447a1a
Terrance Li <tyli@berkeley.edu>
2021-08-30 23:34:06 -0700

    Remove printing of PIDs

--- scripts/start-txn-benchmark.sh
+++ scripts/start-txn-benchmark.sh
@@ -3,21 +3,12 @@ cp conf/anna-txn.yml conf/anna-config.yml
 trap 'kill %1; kill %2; kill %3; kill %4' SIGINT
 
 ./build/target/kvs/anna-monitor &
-MPID=$!
 ./build/target/kvs/anna-route &
-RPID=$!
 
 export SERIALIZABILITY_PROTOCOL="mvcc"
 export SERVER_TYPE="txn"
 ./build/target/kvs/anna-txn &
-S2PID=$!
 
 ./build/target/benchmark/anna-txn-bench &
-BPID=$!
-
-echo $MPID
-echo $RPID
-echo $S2PID
-echo $BPID
 
 ./build/target/benchmark/anna-bench-trigger 1
--- scripts/start-txn.sh
+++ scripts/start-txn.sh
@@ -3,17 +3,10 @@ cp conf/anna-txn.yml conf/anna-config.yml
 trap 'kill %1; kill %2; kill %3' SIGINT
 
 ./build/target/kvs/anna-monitor &
-MPID=$!
 ./build/target/kvs/anna-route &
-RPID=$!
 
 export SERIALIZABILITY_PROTOCOL="mvcc"
 export SERVER_TYPE="txn"
 ./build/target/kvs/anna-txn &
-S2PID=$!
-
-echo $MPID
-echo $RPID
-echo $S2PID
 
 ./build/cli/anna-txn-cli conf/anna-txn.yml
