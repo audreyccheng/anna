b79736328fc9d59ba1d3034c580fbcbc161a2731
Terrance Li <tyli@berkeley.edu>
2021-08-31 18:31:29 -0700

    Quick bugfix

--- scripts/update-configs.py
+++ scripts/update-configs.py
@@ -33,55 +33,55 @@ log_instance = get_instance_by_id(instances, LOG_NODE_INSTANCE_ID)
 def generate_config(txn_public_ip, txn_private, public_ip, private_ip, is_txn=False):
   server_name = 'txn-server' if is_txn else 'server'
   return f"""
-    monitoring:
-      mgmt_ip: {txn_public_ip}
-      ip: {txn_public_ip}
-    routing:
-      monitoring:
-          - {txn_public_ip}
-      ip: {txn_public_ip}
-    user:
-      monitoring:
-          - {txn_public_ip}
-      routing:
-          - {txn_public_ip}
-      ip: {txn_public_ip}
-    {server_name}:
-      monitoring:
-          - {txn_private}
-      routing:
-          - {txn_private}
-      seed_ip: {txn_private}
-      public_ip: {public_ip}
-      private_ip: {private_ip}
-      mgmt_ip: "NULL"
-    policy:
-      elasticity: false
-      selective-rep: false
-      tiering: true
-    ebs: ./
-    capacities: # in GB
-      txn-cap: 1
-      memory-cap: 1
-      ebs-cap: 0
-      log-cap: 1
-    threads:
-      txn: {TXN_THREADS}
-      memory: {MEM_THREADS}
-      ebs: 1
-      log: {LOG_THREADS}
-      routing: 1
-      benchmark: {BENCHMARK_THREADS}
-    replication:
-      txn: 1
-      memory: 1
-      ebs: 0
-      log: 1
-      minimum: 1
-      local: 1
-    benchmark:
-        - localhost
-    """
+monitoring:
+  mgmt_ip: {txn_public_ip}
+  ip: {txn_public_ip}
+routing:
+  monitoring:
+      - {txn_public_ip}
+  ip: {txn_public_ip}
+user:
+  monitoring:
+      - {txn_public_ip}
+  routing:
+      - {txn_public_ip}
+  ip: {txn_public_ip}
+{server_name}:
+  monitoring:
+      - {txn_private}
+  routing:
+      - {txn_private}
+  seed_ip: {txn_private}
+  public_ip: {public_ip}
+  private_ip: {private_ip}
+  mgmt_ip: "NULL"
+policy:
+  elasticity: false
+  selective-rep: false
+  tiering: true
+ebs: ./
+capacities: # in GB
+  txn-cap: 1
+  memory-cap: 1
+  ebs-cap: 0
+  log-cap: 1
+threads:
+  txn: {TXN_THREADS}
+  memory: {MEM_THREADS}
+  ebs: 1
+  log: {LOG_THREADS}
+  routing: 1
+  benchmark: {BENCHMARK_THREADS}
+replication:
+  txn: 1
+  memory: 1
+  ebs: 0
+  log: 1
+  minimum: 1
+  local: 1
+benchmark:
+    - localhost
+"""
 
 txn_config = generate_config(get_private_ip(txn_instance), get_private_ip(txn_instance), get_private_ip(txn_instance), get_private_ip(txn_instance), True)
 mem_config = generate_config(get_public_ip(txn_instance), get_private_ip(txn_instance), get_public_ip(mem_instance), get_private_ip(mem_instance))
@@ -89,13 +89,14 @@ log_config = generate_config(get_public_ip(txn_instance), get_private_ip(txn_ins
 
 # Write the provided config into the instance's ~/anna/conf/anna-config.yml
 def write_config(instance, config):
-  cmd = ['ssh', '-i', '~/west-region-key.pem', f'ubuntu@{get_public_ip(instance)}', 'cat - > anna/conf/anna-config.yml']
+  cmd = ['ssh', '-o', 'StrictHostKeyChecking=accept-new', '-i', '~/west-region-key.pem', f'ubuntu@{get_public_ip(instance)}', 'cat - > anna/conf/anna-config.yml']
   p = subprocess.Popen(cmd, stdin=subprocess.PIPE)
 
-  for chunki in range(0, len(txn_config), 1024):
-    chunk = txn_config[chunki:chunki + 1024]
+  for chunki in range(0, len(config), 1024):
+    chunk = config[chunki:chunki + 1024]
     p.stdin.write(bytearray(chunk, 'utf-8'))
 
+print(mem_config)
 write_config(txn_instance, txn_config)
 write_config(mem_instance, mem_config)
 write_config(log_instance, log_config)
