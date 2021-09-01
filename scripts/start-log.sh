81c245f6e5d57ed9c46fe89c583cb2440ff8dac8
Terrance Li <tyli@berkeley.edu>
2021-08-31 19:08:33 -0700

    Delete extraneous scripts

--- scripts/start-log.sh
+++
@@ -1,3 +0,0 @@
-export SERIALIZABILITY_PROTOCOL="mvcc"
-export SERVER_TYPE="log"
-./build/target/kvs/anna-txn
--- scripts/start-mem.sh
+++
@@ -1,3 +0,0 @@
-export SERIALIZABILITY_PROTOCOL="mvcc"
-export SERVER_TYPE="memory"
-./build/target/kvs/anna-txn
